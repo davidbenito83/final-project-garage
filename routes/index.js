7fcdf4ad32733a5668435e3b49973356d4e9a479
davidbenito83 <david.benito.romanillos@gmail.com>
2020-05-17 14:02:19 +0200

    se quita la ruta Home en el back, de momento no tiene sentido.

--- routes/index.js
+++ routes/index.js
@@ -1,14 +1,14 @@
 const express = require("express");
 const router = express.Router();
 
-router.get("/home", (req, res) => {
-  const data = {
-    name: "David",
-    cities: ["Madrid", "Barcelona", "Murcia"]
-  };
-
-  res.render("home", data);
-});
+// router.get("/home", (req, res) => {
+//   const data = {
+//     name: "David",
+//     cities: ["Madrid", "Barcelona", "Murcia"]
+//   };
+//
+//   res.render("home", data);
+// });
 
 // router.get("/about", (req, res) => {
 //   const data = {
