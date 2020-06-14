c69ba976a57f94f5567bb017061647e97ae7584b
davidbenito83 <david.benito.romanillos@gmail.com>
2020-06-14 19:26:12 +0200

    Cambios en login para seguridad parte 1

--- routes/auth/login.js
+++ routes/auth/login.js
@@ -4,36 +4,20 @@ const bcrypt = require("bcryptjs");
 const router = Express.Router();
 const User = require("../../models/User");
 
-router.get("/", (req, res) => {
-  res.render("login");
-});
-
 router.post("/", async (req, res) => {
   const { email, password } = req.body;
 
   try {
     const user = await User.findOne({ email });
 
-    if (!user) return res.render("login", { error: "el usuario no existe" });
+    if (!user) return res.status(200).json({ error: "el usuario no existe" });
 
     const passwordDB = user.password;
 
     if (!bcrypt.compareSync(password, passwordDB))
-      return res.render("login", { error: "La contraseña no es correcta" });
-    
-    req.session.currentUser = user;
+      return res.status(200).json({ error: "La contraseña no es correcta" });
 
-
-    //AQUÍ VA EL TOKEN DE LA APLICACIÓN PARA TRAER API
-
-    // const getToken = require("../../carmd-token/getToken");
-
-    // const token = await getToken();
-
-    // console.log("Token de acceso------------->>>>>> "+token);
-    
-
-    res.redirect("/cars/all");
+      return res.status(200).redirect('http://localhost:3000/dashboard');
 
   } catch (error) {
     console.log(error);
